6e80f51f32a35a8ddc48e1f4521e3b3159be56f8
Harrison McCord <harrison.mccord@gmail.com>
2017-07-27 17:48:05 -0400

    added some comments

--- bamazon.sql
+++ bamazon.sql
@@ -1,7 +1,8 @@
+--Create new database called bamazon--
 CREATE DATABASE bamazon;
-
+--Tells SQL to use the database bamazon--
 USE bamazon;
-
+--Creating a new table called Products and giving it columns for the following--
 CREATE TABLE products(
 	id INTEGER(11) AUTO_INCREMENT NOT NULL,
 	product_name VARCHAR(100),
