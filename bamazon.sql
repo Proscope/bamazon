b615c591f0e06f844b757307428e641ccf1ea855
Harrison McCord <harrison.mccord@gmail.com>
2017-07-27 18:11:14 -0400

    added products

--- bamazon.sql
+++ bamazon.sql
@@ -8,5 +8,36 @@ CREATE TABLE products(
 	product_name VARCHAR(100),
 	department_name VARCHAR(100),
 	price INTEGER(11) NOT NULL,
-	stock_quantity VARCHAR(100)
+	stock_quantity VARCHAR(100),
+	PRIMARY KEY(id)
 );
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Wilson Burn", "Racquets", 179.99, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Wilson Blade", "Racquets", 209.99, 3);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Babolat Pure Strike", "Racquets", 199.99, 7);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Head Prestige", "Racquets", 189.99, 1);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Wilson Xduty", "Balls", 3.99, 2);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Dunlop Champ", "Balls", 3.49, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Head Pro Penn", "Balls", 2.99, 6);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Nike Cage", "Shoes", 129.99, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Adidas Barricade", "Shoes", 149.99, 9);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Asics Speed", "Shoes", 99.99, 1);
+
